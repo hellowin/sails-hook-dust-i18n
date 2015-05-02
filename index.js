125b5ad7bce5e87fd4910bbe1999f22978eecb75
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2015-05-02 21:31:41 +0700

    add args parameters

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function (sails) {
     initialize: function (cb) {
 
       sails.after('hook:i18n:loaded', function () {
+        //define dust
         var dust = null;
         try {
           dust = require('dust');
@@ -15,14 +16,39 @@ module.exports = function (sails) {
           }
         }
 
+        //define sails i18n
+        var i18n = sails.__;
+
+        //define helpers
         dust.helpers.i18n = function (chunk, context, bodies, params) {
+
+          //define arguments if any
+          var args = [];
+          try {
+            args = JSON.parse(params.args.replace(/'/g, '"'));
+          } catch (err) {
+          }
+
+          //if using body block
+          //TODO: how to use it synchronously?
           if (bodies.block) {
             return chunk.capture(bodies.block, context, function (string, chunk) {
-              var result = sails.__(string);
-
+              args.unshift(string);
+              var result = i18n.apply(context.stack.head, args);
               chunk.end(result);
             });
           }
+          //if using t parameter
+          else if (typeof params.t === 'string') {
+            args.unshift(params.t);
+          }
+          //else
+          else {
+            args.unshift('');
+          }
+
+          var result = i18n.apply(context.stack.head, args);
+          chunk.write(result);
 
           return chunk;
         };
